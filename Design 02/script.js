d68acfe0cac275d6107c4f7fc3bbcf6dcc443480
Developer-Keyithan <sathyjaseelankeyithan@gmail.com>
2024-10-17 16:31:03 +0530

    Added Hero Section

---
+++ Design 02/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Game Developer", "Frontend Developer", "Web Developer", "Graphic Designer"],
+    typeSpeed: 50,
+    backSpeed: 75,
+    backDelay: 1000,
+    loop:true
+});
